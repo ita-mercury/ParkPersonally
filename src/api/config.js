04da1283acf1ce365f5f60dda537755fe8a11973
LIMA10 <549408221@qq.com>
2019-07-30 09:34:27 +0800

    update api

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 // axios.defaults.baseURL = 'http://localhost:8088'
-axios.defaults.baseURL = 'http://10.222.232.11:8888'
+// axios.defaults.baseURL = 'http://10.222.232.11:8888'
+axios.defaults.baseURL = '/api'
 axios.defaults.timeout = 5000
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
